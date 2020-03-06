3ca1668123a0b7d2bb14cb615296881809444c79
Michael Trpak <mrtrpak3@gmail.com>
2020-03-06 16:07:31 -0500

    Updates

---
+++ precache-manifest.3203630026df23856f986509bc727963.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c544e9110bdfd2fea53bd29698a6cd99",
+    "url": "/my-page/index.html"
+  },
+  {
+    "revision": "40301322c0cee04ffd1e",
+    "url": "/my-page/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8bac5adafd445bfc7014",
+    "url": "/my-page/static/js/2.f1434acd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my-page/static/js/2.f1434acd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40301322c0cee04ffd1e",
+    "url": "/my-page/static/js/main.9cb70b47.chunk.js"
+  },
+  {
+    "revision": "fbb05086f9d1cc458aa0",
+    "url": "/my-page/static/js/runtime-main.24308380.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
