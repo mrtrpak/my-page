451488ab689816320072c2f5fc08660b641671fb
Michael Trpak <mrtrpak3@gmail.com>
2020-03-09 22:59:37 -0400

    setting route for bundesliga page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
+import BundesligaPage from "./pages/BundesligaPage";
 
 export default function App() {
   return (
     <HashRouter basename="/">
       <Route exact path="/" component={HomePage} />
       <Route exact path="/gamer" />
-      <Route exact path="/soccer" />
+      <Route exact path="/soccer" component={BundesligaPage} />
     </HashRouter>
   );
 };
