4046faaccad0383c0c5136b5d39df3e91e27f556
Michael Trpak <mrtrpak3@gmail.com>
2020-03-09 22:56:11 -0400

    making footer for homepage

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+import ScrollTopBtn from "../ScrollTopBtn";
+
+import "./style.css";
+
+export default function Footer() {
+  return (
+    <Grid container justify="center" alignItems="center" className="footer">
+      <ScrollTopBtn />
+    </Grid>
+  );
+};
