449cf7cb955177b9aaaf2525d3efa7b0c14b568d
Michael Trpak <mrtrpak3@gmail.com>
2020-03-11 18:55:03 -0400

    adding in component

--- src/pages/GamesPage/index.js
+++ src/pages/GamesPage/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 
+import GamerApi from "../../components/GamerApi";
 import PortfolioBtn from "../../components/PortfolioBtn";
 
 export default function GamesPage() {
   return (
     <div>
+      <GamerApi />
       <PortfolioBtn />
     </div>
-  )
-}
+  );
+};
