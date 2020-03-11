4921c8d0cc44123ff8f665d5d1732fca3b7ba693
Michael Trpak <mrtrpak3@gmail.com>
2020-03-11 18:58:33 -0400

    adding in compnent to route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
+import GamesPage from "./pages/GamesPage";
 import BundesligaPage from "./pages/BundesligaPage";
 
 export default function App() {
   return (
     <HashRouter basename="/">
       <Route exact path="/" component={HomePage} />
-      <Route exact path="/gamer" />
+      <Route exact path="/gamer" component={GamesPage} />
       <Route exact path="/soccer" component={BundesligaPage} />
     </HashRouter>
   );
