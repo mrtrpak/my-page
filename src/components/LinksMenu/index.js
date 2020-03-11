3157441b0b67e6c7c53c4f79f25f7e9d4cf236ae
Michael Trpak <mrtrpak3@gmail.com>
2020-03-10 22:43:58 -0400

    changed the menu list

---
+++ src/components/LinksMenu/index.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { Button, Menu, MenuItem, ListItemIcon, ListItemText, Link } from "@material-ui/core";
+import { AssignmentInd, GitHub, LinkedIn } from "@material-ui/icons";
+
+const StyledMenu = withStyles({
+  paper: {
+    border: '1px solid #d3d4d5',
+  },
+})(props => (
+  <Menu
+    elevation={0}
+    getContentAnchorEl={null}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'center',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'center',
+    }}
+    {...props}
+  />
+));
+
+const StyledMenuItem = withStyles(theme => ({
+  root: {
+    '&:focus': {
+      backgroundColor: theme.palette.primary.main,
+      '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
+        color: theme.palette.common.white,
+      },
+    },
+  },
+}))(MenuItem);
+
+export default function CustomizedMenus() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button style={{ padding: "5px" }}
+        aria-controls="customized-menu"
+        aria-haspopup="true"
+        variant="contained"
+        color="primary"
+        onClick={handleClick}
+      >
+        View Links
+      </Button>
+      <StyledMenu
+        id="customized-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <StyledMenuItem>
+          <ListItemIcon>
+            <GitHub fontSize="small" />
+          </ListItemIcon>
+          <Link style={{ textDecoration: "none" }} color="inherit" href="https://github.com/mrtrpak" target="_blank" rel="noreferrer">
+            <ListItemText primary="Github" />
+          </Link>
+        </StyledMenuItem>
+        <StyledMenuItem>
+          <ListItemIcon>
+            <LinkedIn fontSize="small" />
+          </ListItemIcon>
+          <Link style={{ textDecoration: "none" }} color="inherit" href="https://www.linkedin.com/in/michael-trpak/" target="_blank" rel="noreferrer">
+            <ListItemText primary="LinkedIn" />
+          </Link>
+        </StyledMenuItem>
+        <StyledMenuItem>
+          <ListItemIcon>
+            <AssignmentInd fontSize="small" />
+          </ListItemIcon>
+          <Link style={{ textDecoration: "none" }} color="inherit" href="https://docs.google.com/document/d/1S9v9bpBBEbvUwknThouJwZQDInZZm4TMdNDa69TNYIE/edit?usp=sharing" target="_blank" rel="noreferrer">
+            <ListItemText primary="Resume" />
+          </Link>
+        </StyledMenuItem>
+      </StyledMenu>
+    </div>
+  );
+};
