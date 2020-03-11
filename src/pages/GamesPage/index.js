349d5c6af61a045fca69264bc6e8df36876400a8
Michael Trpak <mrtrpak3@gmail.com>
2020-03-11 16:46:38 -0400

    adding in games page

---
+++ src/pages/GamesPage/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import PortfolioBtn from "../../components/PortfolioBtn";
+
+export default function GamesPage() {
+  return (
+    <div>
+      <PortfolioBtn />
+    </div>
+  )
+}
