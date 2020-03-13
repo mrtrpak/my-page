0bd6c87e8a5278e5391bd4c3ec519b1df31bbe47
Michael Trpak <mrtrpak3@gmail.com>
2020-03-12 23:57:41 -0400

    updating, styling, and making responsive

--- src/components/TopTenGames/index.js
+++ src/components/TopTenGames/index.js
@@ -4,16 +4,18 @@ import { ArrowBackIos, ArrowForwardIos } from "@material-ui/icons";
 import ReactPlayer from "react-player";
 
 import "./style.css";
+import { grey } from "@material-ui/core/colors";
 
 class TopTenGames extends Component {
   constructor(props) {
     super(props);
-    this.state = { currentGame: 10, isDevice: false };
+    this.state = { currentGame: 10, isDevice: false, isHandheld: false, isClicked: "primary", notClicked: "default" };
     this.updateDevice = this.updateDevice.bind(this);
   };
 
   componentDidMount() {
     this.updateDevice();
+    this.numberChosen();
     window.addEventListener("resize", this.updateDevice);
   };
 
@@ -25,28 +27,157 @@ class TopTenGames extends Component {
     let counter = this.state.currentGame;
     counter++;
     if (counter < 11) {
-      this.setState(() => ({ currentGame: counter }));
+      this.setState(() => ({ currentGame: counter }), this.numberChosen());
     } else {
-      this.setState({ currentGame: 1 });
+      this.setState({ currentGame: 1 }, this.numberChosen());
     };
   };
 
   selectNumber = id => {
-    this.setState({ currentGame: id });
+    this.setState({ currentGame: id }, this.numberChosen());
   };
 
   selectNext = () => {
     let counter = this.state.currentGame;
     counter--;
     if (counter > 0) {
-      this.setState(() => ({ currentGame: counter }));
+      this.setState(() => ({ currentGame: counter }), this.numberChosen());
     } else {
-      this.setState({ currentGame: 10 });
+      this.setState({ currentGame: 10 }, this.numberChosen());
     };
   };
 
-  updateDevice() {
-    this.setState({ isDevice: window.innerWidth < 880 });
+  numberChosen = () => {
+    const current = this.state.currentGame;
+
+    switch (current) {
+      case 10:
+        document.getElementById("10").style.background = "skyblue" 
+        document.getElementById("9").style.background = "none"
+        document.getElementById("8").style.background = "none"
+        document.getElementById("7").style.background = "none"
+        document.getElementById("6").style.background = "none"
+        document.getElementById("5").style.background = "none"
+        document.getElementById("4").style.background = "none"
+        document.getElementById("3").style.background = "none"
+        document.getElementById("2").style.background = "none"
+        document.getElementById("1").style.background = "none"
+        return;
+      case 9:
+        document.getElementById("10").style.background = "none" 
+        document.getElementById("9").style.background = "skyblue"
+        document.getElementById("8").style.background = "none"
+        document.getElementById("7").style.background = "none"
+        document.getElementById("6").style.background = "none"
+        document.getElementById("5").style.background = "none"
+        document.getElementById("4").style.background = "none"
+        document.getElementById("3").style.background = "none"
+        document.getElementById("2").style.background = "none"
+        document.getElementById("1").style.background = "none"
+        return;
+      case 8:
+        document.getElementById("10").style.background = "none" 
+        document.getElementById("9").style.background = "none"
+        document.getElementById("8").style.background = "skyblue"
+        document.getElementById("7").style.background = "none"
+        document.getElementById("6").style.background = "none"
+        document.getElementById("5").style.background = "none"
+        document.getElementById("4").style.background = "none"
+        document.getElementById("3").style.background = "none"
+        document.getElementById("2").style.background = "none"
+        document.getElementById("1").style.background = "none"
+        return;
+      case 7:
+        document.getElementById("10").style.background = "none" 
+        document.getElementById("9").style.background = "none"
+        document.getElementById("8").style.background = "none"
+        document.getElementById("7").style.background = "skyblue"
+        document.getElementById("6").style.background = "none"
+        document.getElementById("5").style.background = "none"
+        document.getElementById("4").style.background = "none"
+        document.getElementById("3").style.background = "none"
+        document.getElementById("2").style.background = "none"
+        document.getElementById("1").style.background = "none"
+        return;
+      case 6:
+        document.getElementById("10").style.background = "none" 
+        document.getElementById("9").style.background = "none"
+        document.getElementById("8").style.background = "none"
+        document.getElementById("7").style.background = "none"
+        document.getElementById("6").style.background = "skyblue"
+        document.getElementById("5").style.background = "none"
+        document.getElementById("4").style.background = "none"
+        document.getElementById("3").style.background = "none"
+        document.getElementById("2").style.background = "none"
+        document.getElementById("1").style.background = "none"
+        return;
+      case 5:
+        document.getElementById("10").style.background = "none" 
+        document.getElementById("9").style.background = "none"
+        document.getElementById("8").style.background = "none"
+        document.getElementById("7").style.background = "none"
+        document.getElementById("6").style.background = "none"
+        document.getElementById("5").style.background = "skyblue"
+        document.getElementById("4").style.background = "none"
+        document.getElementById("3").style.background = "none"
+        document.getElementById("2").style.background = "none"
+        document.getElementById("1").style.background = "none"
+        return;
+      case 4:
+        document.getElementById("10").style.background = "none" 
+        document.getElementById("9").style.background = "none"
+        document.getElementById("8").style.background = "none"
+        document.getElementById("7").style.background = "none"
+        document.getElementById("6").style.background = "none"
+        document.getElementById("5").style.background = "none"
+        document.getElementById("4").style.background = "skyblue"
+        document.getElementById("3").style.background = "none"
+        document.getElementById("2").style.background = "none"
+        document.getElementById("1").style.background = "none"
+        return;
+      case 3:
+        document.getElementById("10").style.background = "none" 
+        document.getElementById("9").style.background = "none"
+        document.getElementById("8").style.background = "none"
+        document.getElementById("7").style.background = "none"
+        document.getElementById("6").style.background = "none"
+        document.getElementById("5").style.background = "none"
+        document.getElementById("4").style.background = "none"
+        document.getElementById("3").style.background = "skyblue"
+        document.getElementById("2").style.background = "none"
+        document.getElementById("1").style.background = "none"
+        return;
+      case 2:
+        document.getElementById("10").style.background = "none" 
+        document.getElementById("9").style.background = "none"
+        document.getElementById("8").style.background = "none"
+        document.getElementById("7").style.background = "none"
+        document.getElementById("6").style.background = "none"
+        document.getElementById("5").style.background = "none"
+        document.getElementById("4").style.background = "none"
+        document.getElementById("3").style.background = "none"
+        document.getElementById("2").style.background = "skyblue"
+        document.getElementById("1").style.background = "none"
+        return;
+      case 1:
+        document.getElementById("10").style.background = "none" 
+        document.getElementById("9").style.background = "none"
+        document.getElementById("8").style.background = "none"
+        document.getElementById("7").style.background = "none"
+        document.getElementById("6").style.background = "none"
+        document.getElementById("5").style.background = "none"
+        document.getElementById("4").style.background = "none"
+        document.getElementById("3").style.background = "none"
+        document.getElementById("2").style.background = "none"
+        document.getElementById("1").style.background = "skyblue"
+        return;
+      default:
+        break;
+    };
+  };
+
+  updateDevice = () => {
+    this.setState({ isDevice: window.innerWidth < 880, isHandheld: window.innerWidth < 650 });
   };
 
   render() {
@@ -54,34 +185,51 @@ class TopTenGames extends Component {
     const gameId = this.state.currentGame;
     const conVar = this.props.getData;
     const isDevice = this.state.isDevice;
+    const isHandheld = this.state.isHandheld;
 
     return (
       <Grid container justify="center" alignItems="center">
-        <Grid item container alignItems="center" component={Paper} spacing={1}>
-          <Grid item xs={1}>
-            <IconButton onClick={() => this.selectPrev()} className="backArrow iconBtn" aria-label="last game">
-              <ArrowBackIos className="backArrow" fontSize="large" />
-            </IconButton>
+        { !isHandheld ? (
+          <Grid item container justify="center" alignItems="center" component={Paper} spacing={1}>
+            <Grid item container justify="center" xs={2}>
+              <Grid item>
+              <IconButton onClick={() => this.selectPrev()} aria-label="last game">
+                <ArrowBackIos fontSize="large" />
+              </IconButton>
+              </Grid> 
+            </Grid>
+            <Grid className="btnGroup" item xs={8}>
+              <ButtonGroup variant="text" aria-label="text primary button group">
+                {numbers.map(btnRender => (
+                  <Button id={btnRender} onClick={() => this.selectNumber(btnRender)} key={btnRender}>{btnRender}</Button>
+                ))}
+              </ButtonGroup>
+            </Grid>
+            <Grid item container justify="center" xs={2}>
+              <Grid item>
+                <IconButton onClick={() => this.selectNext()} aria-label="next game">
+                  <ArrowForwardIos fontSize="large" />
+                </IconButton>
+              </Grid>
+            </Grid>
           </Grid>
-          <Grid className="btnGroup" item xs={10}>
-            <ButtonGroup variant="text" aria-label="text primary button group">
-              {numbers.map(btnRender => (
-                <Button onClick={() => this.selectNumber(btnRender)} key={btnRender}>{btnRender}</Button>
-              ))}
-            </ButtonGroup>
+        ):
+          <Grid item container justify="center" alignItems="center" component={Paper} spacing={1}>
+            <Grid className="btnGroup" item xs={12}>
+              <ButtonGroup variant="text" aria-label="text primary button group">
+                {numbers.map(btnRender => (
+                  <Button color={this.numberChosen()} onClick={() => this.selectNumber(btnRender)} key={btnRender}>{btnRender}</Button>
+                ))}
+              </ButtonGroup>
+            </Grid>
           </Grid>
-          <Grid className="iconBtn" item xs={1}>
-            <IconButton onClick={() => this.selectNext()} aria-label="next game">
-              <ArrowForwardIos fontSize="large" />
-            </IconButton>
-          </Grid>
-        </Grid>
+      }
         { !isDevice ? (
           <Grid item container justify="center" alignItems="center" spacing={3} className="gameBody">
             <Grid item xs={12}>
               <h1 className="gameTitle title">{conVar[gameId]["name"]}</h1>
             </Grid>
-            <Grid item container justify="center" alignItems="center" xs={6} component={Paper}>
+            <Grid item container justify="center" alignItems="center" xs={6}>
               <Grid item>
                 <ReactPlayer className="vidPlayer" url={conVar[gameId + 20]} controls width="100%" height="100%" />
               </Grid>
@@ -109,7 +257,7 @@ class TopTenGames extends Component {
             <Grid item xs={12}>
               <h1 className="gameTitle title">{conVar[gameId]["name"]}</h1>
             </Grid>
-            <Grid item container justify="center" alignItems="center" xs={8} component={Paper}>
+            <Grid item container justify="center" alignItems="center" xs={8}>
               <Grid item>
                 <ReactPlayer className="vidPlayer" url={conVar[gameId + 20]} controls width="100%" height="100%" />
               </Grid>
@@ -121,6 +269,11 @@ class TopTenGames extends Component {
               <Grid item xs={8}>
                 <h3 className="rating">{conVar[gameId]["rating"]}</h3>
               </Grid>
+            </Grid>
+            <Grid item xs={12}>
+              <Link href={conVar[gameId]["background_image"]} target="_blank" rel="noopener" style={{ textDecoration: "none" }}>
+                <h4 className="title">Game Background Image</h4>
+              </Link>
             </Grid>
           </Grid>
         }
