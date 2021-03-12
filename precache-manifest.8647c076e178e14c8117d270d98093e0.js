62fa32321cc523baf3a428aeda28fe7609a3ab19
Michael Trpak <mrtrpak3@gmail.com>
2021-03-12 16:37:26 -0500

    Updates

---
+++ precache-manifest.8647c076e178e14c8117d270d98093e0.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84cfaa6435c7ab5f1d3d906b268f7b18",
+    "url": "/my-page/index.html"
+  },
+  {
+    "revision": "0572c62cdff1ee112d0f",
+    "url": "/my-page/static/css/main.4f28b631.chunk.css"
+  },
+  {
+    "revision": "e5583b5497a4f4f932df",
+    "url": "/my-page/static/js/2.96eb5d7d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-page/static/js/2.96eb5d7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0572c62cdff1ee112d0f",
+    "url": "/my-page/static/js/main.bea94951.chunk.js"
+  },
+  {
+    "revision": "24a74e670725a91afbb1",
+    "url": "/my-page/static/js/runtime-main.9afb9fb7.js"
+  },
+  {
+    "revision": "0ee774e05c61b31f591f90c948bfef4d",
+    "url": "/my-page/static/media/brainFace.0ee774e0.gif"
+  },
+  {
+    "revision": "941bb4d713d26bd60a286e4ff1e1afd2",
+    "url": "/my-page/static/media/budgetTracker.941bb4d7.gif"
+  },
+  {
+    "revision": "30a2385bdd19149f2e04a117c52ebbb5",
+    "url": "/my-page/static/media/bundesliga.30a2385b.jpg"
+  },
+  {
+    "revision": "333ff3651c7284809afbb4d690f30891",
+    "url": "/my-page/static/media/businessCard.333ff365.gif"
+  },
+  {
+    "revision": "91e38e6123da85eef7153692dc95f0b0",
+    "url": "/my-page/static/media/games.91e38e61.jpg"
+  },
+  {
+    "revision": "bff4dfd5c1670d582a74b9e32f90dbb5",
+    "url": "/my-page/static/media/germany.bff4dfd5.jpg"
+  },
+  {
+    "revision": "a7b2dd54b84d215eff649f4270bec25c",
+    "url": "/my-page/static/media/logo.a7b2dd54.png"
+  },
+  {
+    "revision": "8a320437a254732b0d5e71b5fc6bac15",
+    "url": "/my-page/static/media/prague.8a320437.jpg"
+  },
+  {
+    "revision": "111dd20d4be8276fea92edfc1076c66d",
+    "url": "/my-page/static/media/switchGif.111dd20d.gif"
+  },
+  {
+    "revision": "71b0bcee091c3eb29081229112c7c8ee",
+    "url": "/my-page/static/media/wood.71b0bcee.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-page/precache-manifest.58a262b88cb052f00480d958fe26c972.js"
+  "/my-page/precache-manifest.8647c076e178e14c8117d270d98093e0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-page/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
